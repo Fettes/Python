525a17cab3fa130a69471e14794b349afc03c5e8
Tianshi Feng <tianshifeng@Tianshis-MacBook-Air.local>
2019-09-06 16:08:12 -0400

    update

---
+++ Socket_Exercise_03/client.py
@@ -0,0 +1,15 @@
+# Import the socket package
+import socket
+
+# create a socket
+s = socket.socket()
+# connect the server
+s.connect(('192.168.200.52', 19002))  # ①
+while True:
+    res = s.recv(1024)
+    str1 = input('>>').strip()
+    if not str1:
+        break
+    s.send(str1.encode('utf-8'))
+    print('My command is：%s' % res.decode('utf-8'))
+s.close()
