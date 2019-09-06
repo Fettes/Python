6922b47fa6cccdfa53bdf6ad7b63e8e782f8c99e
Tianshi Feng <tianshifeng@Tianshis-MacBook-Air.local>
2019-09-06 16:42:16 -0400

    update

--- Socket_Exercise_03/client.py
+++ Socket_Exercise_03/client.py
@@ -1,15 +1,19 @@
 # Import the socket package
 import socket
+from datetime import time
 
 # create a socket
+
 s = socket.socket()
 # connect the server
 s.connect(('192.168.200.52', 19002))  # ①
-while True:
+# Define the command list
+command_list = ["Tianshi Feng","look","look chest","look mirror","get hairpin",
+                "unlock chest with hairpin","open chest","look in chest","unlock door with hairpin",
+                "open door"]
+for i in command_list:
     res = s.recv(1024)
-    str1 = input('>>').strip()
-    if not str1:
-        break
-    s.send(str1.encode('utf-8'))
-    print('My command is：%s' % res.decode('utf-8'))
+    print(res.decode('utf-8'))
+    s.send(command_list[i].encode('utf-8'))
+    time.sleep(0.25)
 s.close()
